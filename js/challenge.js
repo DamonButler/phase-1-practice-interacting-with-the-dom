223bd30029f62ed58e26e9083ffccb499219a703
CollinBurleigh <collin.s.burleigh@gmail.com>
2023-02-02 11:37:45 -0500

    Plus and Minus Buttons Working

--- js/challenge.js
+++ js/challenge.js
@@ -1,7 +1,12 @@
 const minusButton = document.querySelector("#minus")
 const plusButton = document.querySelector("#plus")
 const theCounter = document.querySelector("#counter")
+let i = theCounter.innerText;
 
 minusButton.addEventListener('click', e => {
+    theCounter.innerText = --i;
+})
 
+plusButton.addEventListener('click', e => {
+    theCounter.innerText = ++i;
 })
